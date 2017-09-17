4f62fe12b6f4cb8392eb8ce194076dadbe5e360a
Collin Li <collin.li0499@gmail.com>
2017-09-17 02:21:53 -0400

    Nope command

--- NewEK/src/Cards/Nope.java
+++ NewEK/src/Cards/Nope.java
@@ -2,12 +2,66 @@ package Cards;
 
 import Cards.Card;
 
-public class Nope extends Card
-{
-    public Nope()
-    {
-        super("Negates a card effects!");
-        setName("Nope");
+import java.util.Scanner;
+
+public class Nope {
+
+    private boolean nope = false;
+
+    public boolean Nope() {
+        String no;
+        int pnum;
+        while (true) {
+            setName("Nope");
+            System.out.println("Does anyone want to play a nope? y/n");
+            Scanner scan = new Scanner(System.in);
+            no = scan.next();
+            no = no.toLowerCase();
+            if (no.equals(y)) {
+                System.out.println("Who is playing it?");
+                pnum = scan.nextInt();
+                switch (pnum) {
+                    case 1:
+                        for (int i = 0; i < p1.getph().size(); i++) {
+                            if (c.compareTo("Nope")) {
+                                p1.getph().remove(p1.getph().getCard(i));
+                                nope = !nope;
+                                break;
+                            }
+                        }
+                        break;
+                    case 2:
+                        for (int i = 0; i < p2.getph().size(); i++) {
+                            if (c.compareTo("Nope")) {
+                                p2.getph().remove(p2.getph().getCard(i));
+                                nope = !nope;
+                                break;
+                            }
+                        }
+                        break;
+                    case 3:
+                        for (int i = 0; i < p3.getph().size(); i++) {
+                            if (c.compareTo("Nope")) {
+                                p3.getph().remove(p3.getph().getCard(i));
+                                nope = !nope;
+                                break;
+                            }
+                        }
+                        break;
+                    case 4:
+                        for (int i = 0; i < p4.getph().size(); i++) {
+                            if (c.compareTo("Nope")) {
+                                p4.getph().remove(p4.getph().getCard(i));
+                                nope = !nope;
+                                break;
+                            }
+                        }
+                        break;
+                }
+            } else {
+                return nope;
+            }
+        }
     }
 
 
